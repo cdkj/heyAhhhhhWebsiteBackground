d1797d64a6be999be97c1532219b05252b7f5289
cdkj <cdkj.muqinghe@gmail.com>
2022-05-13 19:10:22 +0800

    initialize project

---
+++ src/main/java/com/heyahhhh/websitebackend/WebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heyahhhh.websitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteBackendApplication.class, args);
+    }
+
+}
